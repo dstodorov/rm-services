508287c82e0f7827c23c60cca12ffedf70eeb750
Dimitar Todorov <d.todorov.vn@gmail.com>
2023-04-16 21:02:48 +0300

    Add getAllProducts mapping

--- inventory-service/src/main/java/com/dst/inventoryservice/controllers/ProductController.java
+++ inventory-service/src/main/java/com/dst/inventoryservice/controllers/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -16,6 +17,20 @@ import java.util.Optional;
 public class ProductController {
     private final ProductService productService;
 
+    @GetMapping
+    public ResponseEntity<List<ProductDTO>> getAllProducts() {
+        Optional<List<ProductDTO>> allProducts = this.productService.getAll();
+
+        return allProducts.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> getProductById(@PathVariable("id") Long id) {
+        Optional<ProductDTO> product = this.productService.getProduct(id);
+
+        return product.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
 
     @PostMapping(value = "/add")
     public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO product, UriComponentsBuilder uriComponentsBuilder) {
@@ -26,11 +41,5 @@ public class ProductController {
 
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ProductDTO> getProductById(@PathVariable("id") Long id) {
-        Optional<ProductDTO> product = this.productService.getProduct(id);
 
-        return product.map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-    }
 }
--- inventory-service/src/main/java/com/dst/inventoryservice/services/ProductService.java
+++ inventory-service/src/main/java/com/dst/inventoryservice/services/ProductService.java
@@ -7,7 +7,9 @@ import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -28,6 +30,14 @@ public class ProductService {
 
     public Optional<ProductDTO> getProduct(Long id) {
         return this.productRepository.findById(id).map(this::mapToProductDTO);
+    }
+
+    public Optional<List<ProductDTO>> getAll() {
+        if (this.productRepository.count() == 0) {
+            return Optional.empty();
+        }
+
+        return Optional.of(this.productRepository.findAll().stream().map(this::mapToProductDTO).toList());
     }
 
     private ProductDTO mapToProductDTO(Product product) {
